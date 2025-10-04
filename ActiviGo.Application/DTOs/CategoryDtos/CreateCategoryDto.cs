59b80ba659446a160bf64c4b0956f6ef34e7f4b5
theMargarita <margotvoistinova@hotmail.se>
2025-10-04 17:50:30 +0200

    created service interface for my part

--- ActiviGo.Application/DTOs/CategoryDtos/CreateCategoryDto.cs
+++ ActiviGo.Application/DTOs/CategoryDtos/CreateCategoryDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace ActiviGo.Application.DTOs.CategoryDtos
 {
-    public class CreateGategoryDto
+    public class CreateCategoryDto
     {
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
---
+++ ActiviGo.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using ActiviGo.Application.DTOs;
+using ActiviGo.Application.DTOs.CategoryDtos;
+using ActiviGo.Domain.Models;
+
+namespace ActiviGo.Application.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryReadDto>> GetAllCategoriesAsync();
+        Task<Category?> GetCategoryByIdAsync(int categoryId, CancellationToken ct);
+        Task<Category?> GetCategoryWithActivities(Guid categoryId, CancellationToken ct);
+        Task<Category> CreateCategoryAsync(CreateCategoryDto createCategoryDto, CancellationToken ct);
+        Task DeleteCategoryAsync(int categoryId);
+        Task<CategoryReadDto> UpdateCategoryAsync(int id, CategoryUpdateDto updateCategoryDto, CancellationToken ct);
+    }
+}
