dd6c12d4056a67903a1de7f69138714f007abe52
theMargarita <margotvoistinova@hotmail.se>
2025-10-04 20:30:30 +0200

    i think i meant on this one

---
+++ ActiviGo.Domain/Interfaces/ICategoryRepository,cs.cs
@@ -0,0 +1,13 @@
+﻿using ActiviGo.Domain.Models;
+
+namespace ActiviGo.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAllCategoriesAsync();
+        Task<IEnumerable<Category>> GetCategoriesWithActivitiesAsync(string activityName);
+        Task<Category>CreateCategoryAsync(Category category);
+        Task<Category>UpdateCategoryAsync(Category category);
+        Task <bool>DeleteCategoryAsync(int categoryId);
+    }
+}
---
+++ ActiviGo.Domain/Interfaces/IZoneRepository.cs
@@ -0,0 +1,14 @@
+﻿using ActiviGo.Domain.Models;
+using System.Threading.Tasks;
+
+namespace ActiviGo.Domain.Interfaces
+{
+    public interface IZoneRepository
+    {
+        Task<IEnumerable<Zone>> GetAllCategoriesAsync();
+        Task<IEnumerable<Zone>> GetCategoriesWithActivitiesAsync(string activityName);
+        Task<Category> CreateCategoryAsync(Zone zone);
+        Task<Category> UpdateCategoryAsync(Zone zone);
+        Task DeleteCategoryAsync(int ZoneId);
+    }
+}
