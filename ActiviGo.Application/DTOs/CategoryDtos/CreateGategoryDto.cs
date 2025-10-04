50bfa25851fef0f8d8431f6afecb1b50c889890c
theMargarita <margotvoistinova@hotmail.se>
2025-10-04 17:14:14 +0200

    added dtos

---
+++ ActiviGo.Application/DTOs/CategoryDtos/CreateGategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ActiviGo.Application.DTOs.CategoryDtos
+{
+    public class CreateGategoryDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
