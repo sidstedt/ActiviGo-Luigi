52d8fef1ba3157ff1f85551a6f57b9d5fed35565
JohanHanssonSUT24 <johanh85@hotmail.com>
2025-10-07 18:36:10 +0200

    Started with AdminController. Changed UoW-Service to IDisposible

---
+++ ActiviGo.WebApi/Controllers/AdminController.cs
@@ -0,0 +1,42 @@
+﻿using ActiviGo.Application.DTOs; // För Activity DTOs
+using ActiviGo.Application.Interfaces;
+using ActiviGo.Domain.Interfaces;
+using ActiviGo.Domain.Models; // För User-modellen
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ActiviGo.WebApi.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole<Guid>> _roleManager;
+
+        private readonly IActivityService _activityService;
+        private readonly IActivityOccurrenceService _activityOccurrenceService;
+
+        private readonly IUnitofWork _uow;
+
+        public AdminController(
+            UserManager<User> userManager,
+            RoleManager<IdentityRole<Guid>> roleManager,
+            IActivityService activityService,
+            IActivityOccurrenceService activityOccurrenceService,
+            IUnitofWork uow)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _activityService = activityService;
+            _activityOccurrenceService = activityOccurrenceService;
+            _uow = uow;
+        }
+    }
+}
