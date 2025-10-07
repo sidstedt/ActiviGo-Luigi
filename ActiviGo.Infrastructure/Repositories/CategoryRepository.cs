19b93da55661e14bd036e979c2c866aa42b9c42c
theMargarita <margotvoistinova@hotmail.se>
2025-10-07 11:47:01 +0200

    added generic and removed soem methods unitofwork, logger,

--- ActiviGo.Infrastructure/Repositories/CategoryRepository.cs
+++ ActiviGo.Infrastructure/Repositories/CategoryRepository.cs
@@ -5,60 +5,17 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ActiviGo.Infrastructure.Repositories
 {
-    public class CategoryRepository : ICategoryRepository
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
     {
-        private readonly ActiviGoDbContext _context;
-        public CategoryRepository(ActiviGoDbContext context)
-        {
-            _context = context;
-        }
-
-        public async Task<Category> CreateCategoryAsync(Category category)
-        {
-            _context.Categories.Add(category);
-            await _context.SaveChangesAsync();
-            return category;
-        }
-
-        public async Task<bool> DeleteCategoryAsync(int categoryId)
-        {
-            var removeCategory = await _context.Categories.FindAsync(categoryId);
-
-            if (removeCategory != null) { return false; }
-
-            _context.Categories.Remove(removeCategory);
-            await _context.SaveChangesAsync();
-            return true;
-        }
-
-        public async Task<IEnumerable<Category>> GetAllCategoriesAsync()
-        {
-            return await _context.Categories.ToListAsync();
-        }
+        public CategoryRepository(ActiviGoDbContext context) : base(context) { }
 
         public async Task<IEnumerable<Category>> GetCategoriesWithActivitiesAsync(int id)
         {
-            return await _context.Categories
-                .Where(c => c.Activities.Any(a => a.Id == id))  
-                .Include(c => c.Activities)
+            return await _dbSet.Include(c =>  c.Activities)
+                .Where(c => c.Activities
+                .Any(a => a.Id == id))  
                 .AsNoTracking()
                 .ToListAsync();
-        }
-
-        public async Task<Category> UpdateCategoryAsync(Category category)
-        {
-            var updateCategory = await _context.Categories.FindAsync(category.Id);
-
-            if (updateCategory is null) return null;
-
-            _context.Categories.Update(updateCategory);
-            await _context.SaveChangesAsync();
-            return category;
-        }
-
-        public async Task<Category?> FindByIdAsync(int id)
-        {
-            return await _context.Categories.FindAsync(id);
         }
     }
 }
