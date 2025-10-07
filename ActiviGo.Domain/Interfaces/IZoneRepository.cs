d47c2cbae4cbc771ef2646b92b1d9d4301f30689
theMargarita <margotvoistinova@hotmail.se>
2025-10-07 22:03:52 +0200

    added new method for zone

--- ActiviGo.Domain/Interfaces/IZoneRepository.cs
+++ ActiviGo.Domain/Interfaces/IZoneRepository.cs
@@ -6,5 +6,19 @@ namespace ActiviGo.Domain.Interfaces
     public interface IZoneRepository : IGenericRepository<Zone>
     {
         Task<IEnumerable<Zone>> GetZoneWithActivitiesAsync(int id);
+
+        //fetch all the zone for the given location id
+        Task<IEnumerable<Zone>> GetZonesByLocationIdAsync(int locationId);
+
+        //fetch all zones with activites and location details
+        Task<IEnumerable<Zone>> GetAllZonesWithActivitiesAndLocationAsync();
+
+
+        //not sure about these two methods, need to discuss
+        // add activity to a zone
+        Task AddActivityToZoneAsync(int zoneId, int activityId);
+
+        // remove activity from a zone
+        Task RemoveActivityFromZoneAsync(int zoneId, int activityId);
     }
 }
