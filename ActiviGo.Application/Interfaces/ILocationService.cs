ea81791c3463b8b4d7ac2dc34977893dead130b5
jasharicoco <jashari.egzon@hotmail.com>
2025-10-08 00:14:02 +0200

    LocationService + interface

---
+++ ActiviGo.Application/Interfaces/ILocationService.cs
@@ -0,0 +1,11 @@
+﻿using ActiviGo.Application.DTOs;
+using ActiviGo.Domain.Models;
+
+namespace ActiviGo.Application.Interfaces
+{
+    public interface ILocationService
+        : IGenericService<Location, LocationResponseDto, LocationCreateDto, LocationUpdateDto>
+    {
+
+    }
+}
