2c5df0522cd1fb11e3026245e5c44c708b1d8512
theMargarita <margotvoistinova@hotmail.se>
2025-10-08 21:48:19 +0200

    added validators

---
+++ ActiviGo.Application/Validators/ZoneValidator.cs
@@ -0,0 +1,22 @@
+﻿using ActiviGo.Application.DTOs;
+using ActiviGo.Domain.Models;
+using FluentValidation;
+
+namespace ActiviGo.Application.Validators
+{
+    public class ZoneValidator : AbstractValidator<ZoneDto>
+    {
+        public ZoneValidator()
+        {
+            RuleFor(z => z.Name)
+                .NotEmpty().WithMessage("Zone name is required.")
+                .MaximumLength(100).WithMessage("Zone name must not exceed 100 characters.");
+
+            RuleFor(z => z.IsOutdoor)
+                .NotNull().WithMessage("IsOutdoor field is required.");
+
+            RuleFor(z => z.LocationId)
+                .GreaterThan(0).WithMessage("LocationId must be a positive integer.");
+        }
+    }
+}
