007501230905dca47fe49bfe9fa457576f2a17f1
JohanHanssonSUT24 <johanh85@hotmail.com>
2025-10-09 10:09:13 +0200

    Added AdminController

--- ActiviGo.WebApi/Controllers/AdminController.cs
+++ ActiviGo.WebApi/Controllers/AdminController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -37,6 +38,80 @@ namespace ActiviGo.WebApi.Controllers
             _activityService = activityService;
             _activityOccurrenceService = activityOccurrenceService;
             _uow = uow;
+        }
+
+        [HttpPost("activities")]
+        public async Task<ActionResult<ActivityResponseDto>> CreateActivity([FromBody] ActivityCreateDto createDto)
+        {
+            var activity = await _activityService.CreateAsync(createDto);
+            return CreatedAtAction(nameof(GetActivity), new { id = activity.Id }, activity);
+        }
+        [HttpDelete("activities/{id}")]
+        public async Task<IActionResult> DeleteActivity(int id)
+        {
+            var result = await _activityService.DeleteAsync(id);
+            if (!result == null)
+            {
+                return NotFound($"Activity med ID {id} hittades inte.");
+            }
+            return NoContent();
+        }
+        [HttpGet("activities/{id}")]
+        [AllowAnonymous]
+        public async Task<ActionResult<ActivityResponseDto>> GetActivity(int id)
+        {
+            var activity = await _activityService.GetByIdAsync(id);
+            if (activity == null) return NotFound();
+            return Ok(activity);
+        }
+        [HttpGet("users")]
+        public async Task<ActionResult> GetAllUsers()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            return Ok(users);
+            var userList = new List<object>();
+            foreach (var user in users)
+            {
+                userList.Add(new
+                {
+                    user.Id,
+                    user.Email,
+                    Roles = await _userManager.GetRolesAsync(user)
+                });
+            }
+            return Ok(userList);
+        }
+        [HttpPost("users/{userId}/roles({roleName}")]
+        public async Task<IActionResult> AddRoleToUser(Guid userId, string roleName)
+        {
+            var user = await _userManager.FindByIdAsync(userId.ToString());
+            if (user == null)
+            {
+                return NotFound($"Användare hittades inte.");
+            }
+
+            if (roleName.Equals("Admin", StringComparison.OrdinalIgnoreCase))
+            {
+                return Forbid("Administratörsrollen måste tilldeas manuellt eller via én dedikerad Admin-endpoint.");
+            }
+            if (!await _roleManager.RoleExistsAsync(roleName))
+            {
+                return BadRequest($"Rollen '{roleName}' existerar inte.");
+            }
+            var result = await _userManager.AddToRoleAsync(user, roleName);
+            if (result.Succeeded)
+            {
+                return Ok($"Rollen '{roleName}' tilldelades användare '{user.Email}'.");
+            }
+            return BadRequest(result.Errors);
+        }
+        [HttpPost("zones")]
+        public async Task<IActionResult> CreateNewZone([FromBody] Zone newZone)
+        {
+            await _uow.Zone.AddAsync(newZone);
+            await _uow.SaveChangesAsync();
+
+            return CreatedAtAction("GetZone", new { id = newZone.Id }, newZone);
         }
     }
 }
